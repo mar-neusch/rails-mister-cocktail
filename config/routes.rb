c95ad716f30f1937a81ee157bf21c99472b94087
Marc Neuschwander <marc.neusch@icloud.com>
2020-11-19 16:19:40 +0100

    done, plus que le css à fiare

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:show, :index, :new, :create] do
     resources :doses, only: [:create, :new]
   end
+  resources :doses, only: [:destroy]
 end
