b58eb81d6b103793af6ffa1ab32026f8696fefe5
Marc Neuschwander <marc.neusch@icloud.com>
2020-11-19 12:25:39 +0100

    manual seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+puts 'starting the seed'
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "sugar syrup")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "grenadine")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "orange Juice")
+Ingredient.create(name: "pepper")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "basilic")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "redbull")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "amaretto")
+
+puts "done"
